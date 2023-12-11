857eb7feaf88f54daa2eb234661510b17e3b67fb
nas3423 <134296851+nas3423@users.noreply.github.com>
2023-12-12 05:03:13 +0800

    Update main.tf

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ terraform {
 
     backend "s3"{
         bucket  = "sctp-ce3-tfstate-bucket"
-        key     = "zaw-miniproject.tfstate"
+        key     = "nas-miniproject.tfstate"
         region  = "us-east-1"
     }
 }
